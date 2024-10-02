59ea6a142819062b3bb7ba0d7ba6b88f1a5163ca
danielnifty1 <danielobichere@gmail.com>
2024-10-02 17:24:48 +0100

    corsr correction

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
 
 async function bootstrap() {
+
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('v1');
+
+  app.enableCors({origin: ["http://127.0.0.1:5174",'http://localhost:5174/',"https://vitfit-v2.netlify.app","https://vitfitcoaching.com","http://localhost:5174"]});
+
+  await app.listen(8080);
 }
 bootstrap();
